83f8a1061c6c2ae748e2e9bc8a2429f7a63bba92
pavanagrawal123 <pavanagrawal@outlook.com>
2018-09-08 12:01:40 -0500

    sign in text change

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,7 +4,7 @@ export default class Result extends React.Component {
     render() {
         return (
             <div>
-                Click <span onClick={() => Router.push('/SignIn')}>here</span> to read more
+                Click <span onClick={() => Router.push('/SignIn')}>here</span> to sign in 
           </div>
         )
     }
