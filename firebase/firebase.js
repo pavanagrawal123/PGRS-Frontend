8ea6300a25932776879c00abbcabcab16215da22
pavanagrawal123 <pavanagrawal@outlook.com>
2018-09-08 12:10:27 -0500

    auth key changne

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 import 'firebase/auth/dist/index.cjs';
 
 const config = {
-    apiKey: "AIzaSyB9nwMfWMjSNFPEAm21I3H1FQv5vN-tHPI",
-    authDomain: "testproject-497b2.firebaseapp.com",
-    databaseURL: "https://testproject-497b2.firebaseio.com",
-    projectId: "testproject-497b2",
-    storageBucket: "testproject-497b2.appspot.com",
-    messagingSenderId: "956386037705"
-};
+    apiKey: "AIzaSyDGAnL_vCc7jS1xjvPUZw813nrFp2L8Uiw",
+    authDomain: "fir-intgeration.firebaseapp.com",
+    databaseURL: "https://fir-intgeration.firebaseio.com",
+    projectId: "fir-intgeration",
+    storageBucket: "fir-intgeration.appspot.com",
+    messagingSenderId: "634022199959"
+}
 if (!firebase.apps.length) {
     firebase.initializeApp(config);
 }
