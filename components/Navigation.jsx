77e00d764ee8cf697fdac65fcc5f194c302ef29c
pavanagrawal123 <pavanagrawal@outlook.com>
2018-09-08 11:53:56 -0500

    navigation chane

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,7 +4,7 @@ export default class Result extends React.Component {
     render() {
         return (
             <div>
-                Click <span onClick={() => Router.push('/signin')}>here</span> to signin
+                Click <span onClick={() => Router.push('/SignIn')}>here</span> to read more
           </div>
         )
     }
